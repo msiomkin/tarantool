0f737d0b5d137b924914f0ef48cb7e32624e3f76
Mergen Imeev <imeevma@gmail.com>
2019-03-06 22:33:10 +0300

    sql: remove field nErr from struct Parse
    At the moment, the only purpose of the field nErr of struct Parse
    is to show whether the field is_aborted of the same struct is true
    or false. Let's remove it.
    
    Part of #3965

--- src/box/sql/tokenize.c
+++ src/box/sql/tokenize.c
@@ -511,7 +511,7 @@ sqlRunParser(Parse * pParse, const char *zSql)
 				diag_set(ClientError, ER_SQL_UNKNOWN_TOKEN,
 					 pParse->sLastToken.n,
 					 pParse->sLastToken.z);
-				sql_parser_error(pParse);
+				pParse->is_aborted = true;
 				break;
 			}
 		} else {
@@ -533,7 +533,7 @@ sqlRunParser(Parse * pParse, const char *zSql)
 		pParse->is_aborted = true;
 	if (pParse->is_aborted && pParse->zErrMsg == 0)
 		pParse->zErrMsg = sqlMPrintf(db, "%s", tarantoolErrorMessage());
-	if (pParse->pVdbe != NULL && pParse->nErr > 0) {
+	if (pParse->pVdbe != NULL && pParse->is_aborted) {
 		sqlVdbeDelete(pParse->pVdbe);
 		pParse->pVdbe = 0;
 	}
